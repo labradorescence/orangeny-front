6479da6aa544060f4e8063506101bab5b7e81860
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-09-29 16:42:03 -0400

    1

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    HELLO TING TING
   </React.StrictMode>,
   document.getElementById('root')
 );
