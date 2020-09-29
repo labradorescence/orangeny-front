3079558b6d7bd084c955aaabda2a9b2061b8cb3a
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-09-29 16:25:50 -0400

    initial commit

---
+++ build/precache-manifest.87bb8d7a39148da7a7a1daadfcde77cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cadb70b8c7c2fc391ed5a3e112dd1bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83e4aef21764dd511523",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e2a5284fdc908aa30c8",
+    "url": "/static/js/2.84d25d3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84d25d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e4aef21764dd511523",
+    "url": "/static/js/main.7cc31e0f.chunk.js"
+  },
+  {
+    "revision": "37dac25897e7b74e3e66",
+    "url": "/static/js/runtime-main.f0e2574e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
